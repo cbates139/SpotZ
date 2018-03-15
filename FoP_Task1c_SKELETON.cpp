c2eebc3bae4b61da3bbb108058349a113bc7733f
Callum Batess <calbat@live.co.uk>
2018-03-15 11:42:25 +0000

    Add files via upload

---
+++ FoP_Task1c_SKELETON.cpp
@@ -0,0 +1,348 @@
+//---------------------------------------------------------------------------
+//Program: Skeleton for Task 1c – group assignment
+//Author: Pascale Vacher
+//Last updated: 26 February 2018
+//---------------------------------------------------------------------------
+
+//Go to 'View > Task List' menu to open the 'Task List' pane listing the initial amendments needed to this program
+
+//---------------------------------------------------------------------------
+//----- include libraries
+//---------------------------------------------------------------------------
+
+//include standard libraries
+#include <iostream>	
+#include <iomanip> 
+#include <conio.h> 
+#include <cassert> 
+#include <string>
+#include <sstream>
+using namespace std;
+
+//include our own libraries
+#include "RandomUtils.h"    //for Seed, Random
+#include "ConsoleUtils.h"	//for Clrscr, Gotoxy, etc.
+
+//---------------------------------------------------------------------------
+//----- define constants
+//---------------------------------------------------------------------------
+
+//defining the size of the grid
+const int  SIZEX(25);    	//horizontal dimension
+const int  SIZEY(15);		//vertical dimension
+//defining symbols used for display of the grid and content
+const char SPOT('@');   	//spot
+const char TUNNEL(' ');    	//tunnel
+const char WALL('#');    	//border
+const char PILL('*');		//pill
+const char ZOMBIE('Z');		//zombie
+//defining the command letters to move the spot on the maze
+const int  UP(72);			//up arrow
+const int  DOWN(80); 		//down arrow
+const int  RIGHT(77);		//right arrow
+const int  LEFT(75);		//left arrow
+//defining the other command letters
+const char QUIT('Q');		//to end the game
+
+struct Item {
+	int x, y;
+	char symbol;
+};
+
+//---------------------------------------------------------------------------
+//----- run game
+//---------------------------------------------------------------------------
+
+int main()
+{
+	//function declarations (prototypes)
+	
+	void initialiseGame(char g[][SIZEX], char m[][SIZEX], Item& spot, Item& pill);
+	void paintGame(const char g[][SIZEX], string mess);
+	bool wantsToQuit(const int key);
+	bool isArrowKey(const int k);
+	int  getKeyPress();
+	void updateGameData(const char g[][SIZEX], Item& spot, const int key, string& mess);
+	void updateGrid(char g[][SIZEX], const char m[][SIZEX], const Item spot);
+	void endProgram();
+	
+	//local variable declarations 
+	char grid[SIZEY][SIZEX];			//grid for display
+	char maze[SIZEY][SIZEX];			//structure of the maze
+	Item spot = { 0, 0, SPOT }; 		//spot's position and symbol
+	Item pill = { 0, 0, PILL };			//pill's position and symbol
+	Item zombie = { 0, 0, ZOMBIE };		//zombie's position and symbol
+	string message("LET'S START...");	//current message to player
+
+	//action...
+	Seed();								//seed the random number generator
+	SetConsoleTitle("Spot and Zombies Game - FoP 2017-18");
+	
+	//set this to be called in another function"initialiseEntryScreen" which displays the message and asks the user for their name
+	//
+	initialiseGame(grid, maze, spot);	//initialise grid (incl. walls and spot) 
+	//
+	//
+	paintGame(grid, message);			//display game info, modified grid and messages
+	int key;							//current key selected by player
+	do {
+		
+		key = getKeyPress(); 	//read in  selected key: arrow or letter command
+		if ((isArrowKey(key)) || (wantsToQuit(key)))
+		{
+			updateGameData(grid, spot, key, message);		//move spot in that direction
+			updateGrid(grid, maze, spot);					//update grid information
+		}
+		else
+			message = "INVALID KEY!";	//set 'Invalid key' message
+		paintGame(grid, message);		//display game info, modified grid and messages
+	} while (!wantsToQuit(key));		//while user does not want to quit
+	endProgram();						//display final message
+	return 0;
+}
+
+
+//---------------------------------------------------------------------------
+//----- initialise game state
+//---------------------------------------------------------------------------
+//TODO: Initialise game entry screen
+void initialiseEntryScreen()
+{
+	
+}
+
+void initialiseGame(char grid[][SIZEX], char maze[][SIZEX], Item& spot)
+{ //initialise grid and place spot in middle
+	void setInitialMazeStructure(char maze[][SIZEX]);
+	void setItemInitialCoordinates(Item& spot);
+	void updateGrid(char g[][SIZEX], const char m[][SIZEX], Item b);
+
+	setInitialMazeStructure(maze);		//initialise maze
+	setItemInitialCoordinates(spot);	//spot initial coordinates
+	updateGrid(grid, maze, spot);		//prepare grid
+}
+
+void setItemInitialCoordinates(Item& spot, Item& pill)
+{ //set spot coordinates inside the grid at random at beginning of game
+//TODO: Spot should not spwan on inner walls
+	spot.y = Random(SIZEY - 2);      //vertical coordinate in range [1..(SIZEY - 2)]
+	spot.x = Random(SIZEX - 2);      //horizontal coordinate in range [1..(SIZEX - 2)]
+
+	pill.y = Random(SIZEY - 2);
+	pill.x = Random(SIZEX - 2);
+	if (pill.y == spot.y)
+	{
+		pill.y = Random(SIZEY - 2);
+	}
+} 
+
+
+
+void setInitialMazeStructure(char maze[][SIZEX])
+{ //set the position of the walls in the maze
+
+  //initialise maze configuration
+	char initialMaze[SIZEY][SIZEX] 	//local array to store the maze structure
+		= { 
+			{ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+			{ '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
+		 };
+	//with '#' for wall, ' ' for tunnel, etc. 
+	//copy into maze structure with appropriate symbols
+	for (int row(0); row < SIZEY; ++row)
+		for (int col(0); col < SIZEX; ++col)
+			switch (initialMaze[row][col])
+			{
+			//not a direct copy, in case the symbols used change
+			case '#': maze[row][col] = WALL; break;
+			case ' ': maze[row][col] = TUNNEL; break;
+			}
+}
+
+//---------------------------------------------------------------------------
+//----- update grid state
+//---------------------------------------------------------------------------
+
+//TODO: Powerpill and spot interaction, zombies movement
+void updateGrid(char grid[][SIZEX], const char maze[][SIZEX], const Item spot)
+{ //update grid configuration after each move
+	void setMaze(char g[][SIZEX], const char b[][SIZEX]);
+	void placeItem(char g[][SIZEX], const Item spot;
+
+	setMaze(grid, maze);	//reset the empty maze configuration into grid
+	placeItem(grid, spot);	//set spot in grid
+}
+
+void setMaze(char grid[][SIZEX], const char maze[][SIZEX])
+{ //reset the empty/fixed maze configuration into grid
+	for (int row(0); row < SIZEY; ++row)
+		for (int col(0); col < SIZEX; ++col)
+			grid[row][col] = maze[row][col];
+}
+//TODO: Add zombiees, pills and holes
+void placeItem(char g[][SIZEX], const Item item)
+{ //place item at its new position in grid
+	g[item.y][item.x] = item.symbol;
+}
+
+//---------------------------------------------------------------------------
+//----- move items on the grid
+//---------------------------------------------------------------------------
+void updateGameData(const char g[][SIZEX], Item& spot, const int key, string& mess)
+{ //move spot in required direction
+	bool isArrowKey(const int k);
+	void setKeyDirection(int k, int& dx, int& dy);
+	assert(isArrowKey(key));
+
+	//reset message to blank
+	mess = "                                         ";		//reset message to blank
+
+	//calculate direction of movement for given key
+	int dx(0), dy(0);
+	setKeyDirection(key, dx, dy);
+
+	//check new target position in grid and update game data (incl. spot coordinates) if move is possible
+	switch (g[spot.y + dy][spot.x + dx])
+	{			//...depending on what's on the target position in grid...
+	case TUNNEL:		//can move
+		spot.y += dy;	//go in that Y direction
+		spot.x += dx;	//go in that X direction
+		break;
+	case WALL:  		//hit a wall and stay there
+
+
+		mess = "CANNOT GO THERE!";
+		break;
+	}
+}
+//---------------------------------------------------------------------------
+//----- process key
+//---------------------------------------------------------------------------
+void setKeyDirection(const int key, int& dx, int& dy)
+{ //calculate direction indicated by key
+
+	bool isArrowKey(const int k);
+	assert(isArrowKey(key));
+	switch (key)	//...depending on the selected key...
+	{
+	case LEFT:  	//when LEFT arrow pressed...
+		dx = -1;	//decrease the X coordinate
+		dy = 0;
+		break;
+	case RIGHT: 	//when RIGHT arrow pressed...
+		dx = +1;	//increase the X coordinate
+		dy = 0;
+		break;
+	case UP:
+		dx = 0;
+		dy = -1;
+		break;
+	case DOWN:
+		dx = 0;
+		dy = +1;
+		break;
+	}
+}
+
+int getKeyPress()
+{ //get key or command (in uppercase) selected by user
+  //KEEP THIS FUNCTION AS GIVEN
+	int keyPressed;
+	keyPressed = _getch();			//read in the selected arrow key or command letter
+	while (keyPressed == 224) 		//ignore symbol following cursor key
+		keyPressed = _getch();
+	return 	(toupper(keyPressed));//return it in uppercase 
+}
+
+bool isArrowKey(const int key)
+{	//check if the key pressed is an arrow key (also accept 'K', 'M', 'H' and 'P')
+
+	return (key == LEFT) || (key == RIGHT) || (key == UP) || (key == DOWN);
+}
+bool wantsToQuit(const int key)
+{	//check if the user wants to quit (when key is 'Q' or 'q')
+	return key == QUIT;
+	
+
+}
+
+//---------------------------------------------------------------------------
+//----- display info on screen
+//---------------------------------------------------------------------------
+
+string tostring(int x)
+{	//convert an integer to a string
+	std::ostringstream os;
+	os << x;
+	return os.str();
+}
+string tostring(char x) 
+{	//convert a char to a string
+	std::ostringstream os;
+	os << x;
+	return os.str();
+}
+void showMessage(const WORD backColour, const WORD textColour, int x, int y, const string message)
+{	//display a string using specified colour at a given position 
+	Gotoxy(x, y);
+	SelectBackColour(backColour);
+	SelectTextColour(textColour);
+	cout << message;
+}
+void paintGame(const char g[][SIZEX], string mess)
+{ //display game title, messages, maze, spot and other items on screen
+	string tostring(char x);
+	void showMessage(const WORD backColour, const WORD textColour, int x, int y, const string message);
+	void paintGrid(const char g[][SIZEX]);
+//TODO: Change the colour of the messages
+	//display game title
+	showMessage(clBlack, clGreen, 0, 0, "  SpotZ " );
+//TODO: Date and time should be displayed from the system
+	showMessage(clWhite, clRed, 40, 0, "FoP Task 1c: February 2018" );
+
+	//display menu options available
+	showMessage(clRed, clYellow, 40, 3, "TO MOVE USE KEYBOARD ARROWS ");
+	showMessage(clRed, clYellow, 40, 4, "TO QUIT ENTER 'Q'           ");
+
+	//print auxiliary messages if any
+	showMessage(clBlack, clWhite, 40, 8, mess);	//display current message
+
+
+	showMessage(clRed, clYellow, 10, 25, "SE -- SE1_2 -- Callum Bates & Peter Drega");
+	//display grid contents
+	paintGrid(g);
+}
+
+void paintGrid(const char g[][SIZEX])
+{ //display grid content on screen
+	SelectBackColour(clBlack);
+	SelectTextColour(clWhite);
+	Gotoxy(0, 2);
+	for (int row(0); row < SIZEY; ++row)
+	{
+		for (int col(0); col < SIZEX; ++col)
+			cout << g[row][col];	//output cell content
+		cout << endl;
+	}
+}
+
+void endProgram()
+{
+	void showMessage(const WORD backColour, const WORD textColour, int x, int y, const string message);
+	showMessage(clRed, clYellow, 40, 8, "");	
+	system("pause");	//hold output screen until a keyboard key is hit
+	
+}
